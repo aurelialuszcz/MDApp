22e10bfdaeb943255f59adfe24060600bb3bd42e
aurelialuszcz <aurelia.luszcz@student.griffith.ie>
2023-11-16 15:01:20 +0000

    recipes in boxes

--- app/src/main/java/com/example/assignmentmain/ExistingRecipes.kt
+++ app/src/main/java/com/example/assignmentmain/ExistingRecipes.kt
@@ -4,10 +4,16 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.ui.Alignment
@@ -15,29 +21,58 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 
 class ExistingRecipes : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             val burgerImg = painterResource(id = R.drawable.burger)
+            val pizzaImg = painterResource(id = R.drawable.pizza)
             Surface(modifier = Modifier.fillMaxSize(),
                 color = Color.LightGray) {
                 Column(
                     verticalArrangement = Arrangement.Center,
                     horizontalAlignment = Alignment.CenterHorizontally
                 ) {
-                    // Recipe title
-                    Text("Burger", color = Color.DarkGray)
-                    // Recipe image
-                    Image(
-                        painter = burgerImg,
-                        contentDescription = null,
-                        modifier = Modifier.fillMaxWidth(),
-                        contentScale = ContentScale.FillWidth
-                    )
-                    // Recipe text
-                    Text("Cook burger, stack toppings in a bun, voila!")
+                    Box (modifier = Modifier
+                        .background(Color.LightGray)
+                        .width(250.dp)
+                        .height(250.dp)) {
+                        Column {
+
+                            // Recipe title
+                            Text("Burger", color = Color.DarkGray)
+                            // Recipe image
+                            Image(
+                                painter = burgerImg,
+                                contentDescription = null,
+                                modifier = Modifier.fillMaxWidth(),
+                                contentScale = ContentScale.FillWidth
+                            )
+                            // Recipe text
+                            Text("Cook burger, stack toppings in a bun, voila!")
+                        }
+                    }
+                    Box (modifier = Modifier
+                        .background(Color.LightGray)
+                        .width(250.dp)
+                        .height(250.dp)
+                        .padding(10.dp)){
+                        Column {
+                            // Recipe title
+                            Text("Pizza", color = Color.DarkGray)
+                            // Recipe image
+                            Image(
+                                painter = pizzaImg,
+                                contentDescription = null,
+                                modifier = Modifier.fillMaxWidth(),
+                                contentScale = ContentScale.FillWidth
+                            )
+                            // Recipe text
+                            Text("Stack toppings on a pizza, voila!")
+                        }
+                    }
                 }
             }
         }
