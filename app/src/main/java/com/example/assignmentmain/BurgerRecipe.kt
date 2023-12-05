3383e8b56e69cd2203cd1d6f44a4f2d438032f55
aurelialuszcz <aurelia.luszcz@student.griffith.ie>
2023-12-05 19:05:08 +0000

    parallax effect

--- app/src/main/java/com/example/assignmentmain/BurgerRecipe.kt
+++ app/src/main/java/com/example/assignmentmain/BurgerRecipe.kt
@@ -4,15 +4,21 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
@@ -23,6 +29,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontFamily
@@ -32,6 +39,8 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.assignmentmain.ui.theme.AssignmentMainTheme
+import kotlin.math.min
+import androidx.compose.ui.unit.min as min
 
 class BurgerRecipe : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -46,6 +55,8 @@ class BurgerRecipe : ComponentActivity() {
                     contentDescription = "background",
                     contentScale = ContentScale.FillHeight,
                 )
+                Parallax()
+            }/*
                 Column(
                     modifier = Modifier
                         .fillMaxSize()
@@ -76,7 +87,64 @@ class BurgerRecipe : ComponentActivity() {
                     // Recipe text
                     Text("Burger recipe goes here", color = Color.White)
                 }
-            }
+            }*/
         }
     }
 }
+
+@Composable
+fun Parallax() {
+    val scrollState = rememberScrollState()
+
+    Box{
+        Column (modifier = Modifier
+            .fillMaxSize()
+            .verticalScroll(scrollState)){
+
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(500.dp)
+                    .graphicsLayer {
+                        alpha = 1f - (scrollState.value.toFloat() / scrollState.maxValue)
+                        translationY = 0.5f * scrollState.value
+                    }, contentAlignment = Alignment.Center
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.burger),
+                    contentDescription = null,
+                    modifier = Modifier.fillMaxHeight(),
+                    contentScale = ContentScale.Crop
+                )
+            }
+            Text("Beef burger",
+                modifier = Modifier.padding(horizontal = 8.dp, vertical = 4.dp), color = Color.White)
+
+            Text("Beef recipeBeef recipeBeef recipeBeef recipeBeef recipeBeef recipeBeef recipe" +
+                    "Beef recipeBeef recipeBeef recipeBeef recipeBeef recipeBeef recipeBeef recipeBeef recipeBeef recipe" +
+                    "Beef recipeBeef recipeBeef recipeBeef recipeBeef recipeBeef recipeBeef recipe" +
+                    "Beef recipeBeef recipeBeef recipeBeef recipeBeef recipeBeef recipeBeef recipe" +
+                    "Beef recipeBeef recipeBeef recipeBeef recipeBeef recipeBeef recipeBeef recipeBeef recipeBeef recipeBeef recipeBeef recipeBeef recipeBeef recipe" +
+                    "Beef recipeBeef recipeBeef recipeBeef recipeBeef recipeBeef recipeBeef recipeBeef recipeBeef recipeBeef recipe" +
+                    "Beef recipeBeef recipeBeef recipeBeef recipeBeef recipeBeef recipeBeef recipe" +
+                    "Beef recipeBeef recipeBeef recipeBeef recipeBeef recipeBeef recipeBeef recipeBeef recipe" +
+                    "Beef recipeBeef recipeBeef recipeBeef recipeBeef recipeBeef recipeBeef recipeBeef recipe" +
+                    "Beef recipeBeef recipeBeef recipeBeef recipeBeef recipeBeef recipeBeef recipeBeef recipe" +
+                "Beef recipeBeef recipeBeef recipeBeef recipeBeef recipeBeef recipeBeef recipeBeef recipe" + "Beef recipeBeef recipeBeef recipeBeef recipeBeef recipeBeef recipeBeef recipeBeef recipe"
+                +"Beef recipeBeef recipeBeef recipeBeef recipeBeef recipeBeef recipeBeef recipeBeef recipe"+"Beef recipeBeef recipeBeef recipeBeef recipeBeef recipeBeef recipeBeef recipeBeef recipe"+
+                    "Beef recipeBeef recipeBeef recipeBeef recipeBeef recipeBeef recipeBeef recipeBeef recipe"+"Beef recipeBeef recipeBeef recipeBeef recipeBeef recipeBeef recipeBeef recipeBeef recipe",
+                modifier = Modifier.padding(8.dp), color = Color.White)
+
+        }
+
+        Box (modifier = Modifier
+            .fillMaxWidth()
+            .height(55.dp).graphicsLayer {
+                                         alpha = min(1f, (scrollState.value.toFloat() / scrollState.maxValue.toFloat()))
+            }.background(color = Color.DarkGray),
+            contentAlignment = Alignment.Center
+        ) {
+            Text(text = "Beef burger")
+        }
+        }
+    }
