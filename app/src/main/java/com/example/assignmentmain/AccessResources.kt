e1d0f3d98bf98276def0a5f3108d6200728c2195
aurelialuszcz <aurelia.luszcz@student.griffith.ie>
2023-12-13 10:53:44 +0000

    final commit

--- app/src/main/java/com/example/assignmentmain/AccessResources.kt
+++ app/src/main/java/com/example/assignmentmain/AccessResources.kt
@@ -4,7 +4,6 @@ import android.app.SearchManager
 import android.content.ActivityNotFoundException
 import android.content.Intent
 import android.os.Bundle
-import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
